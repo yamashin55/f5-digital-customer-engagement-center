40d5f3fc5b5ac7b90cbb2fc3d01f0bbe4ee8fdca
yamashin55 <g.shingo.001@gmail.com>
2021-04-17 19:36:36 +0900

    update output

--- solutions/delivery/application_delivery_controller/nginx/kic/aws/outputs.tf
+++ solutions/delivery/application_delivery_controller/nginx/kic/aws/outputs.tf
@@ -22,14 +22,14 @@ output "publicSubnetAZ2" {
   value = module.aws_network.subnetsAz2.public
 }
 
-output "COMMAND1" {
-  value = "aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin ${aws_ecr_repository.ecr.repository_url}"
+output "ECR_is_used_as_registry" {
+  value = "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${aws_ecr_repository.ecr.repository_url}"
 }
 
-output "COMMAND2" {
-  value = "aws eks --region us-west-2 update-kubeconfig --name ${module.eks.cluster_id}"
+output "Update_kubeconfig" {
+  value = "aws eks --region us-east-1 update-kubeconfig --name ${module.eks.cluster_id}"
 }
 
-output "COMMAND3" {
-  value = "aws ec2 create-tags --resources ${module.aws_network.subnetsAz1.public} ${module.aws_network.subnetsAz2.public} --tags Key=kubernetes.io/cluster/${${module.eks.cluster_id}},Value=shared   Key=kubernetes.io/role/elb,Value=1"
+output "Update_the_Subnet_Tags_for_EKS_cluster" {
+  value = "aws ec2 create-tags --resources ${module.aws_network.subnetsAz1.public} ${module.aws_network.subnetsAz2.public} --tags Key=kubernetes.io/cluster/${module.eks.cluster_id},Value=shared   Key=kubernetes.io/role/elb,Value=1"
 }
